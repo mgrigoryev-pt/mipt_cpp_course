8ef2fe78e6ef873d9905f78b2de8cfd0cd7bb56d
akio <akio@akio.com>
2026-09-14 09:43:15 +0300

    feat: task1.1

--- project/src/main.cpp
+++ project/src/main.cpp
@@ -15,10 +15,24 @@
 #include <fstream>
 #include <print>
 #include <string>
+#include <vector>
+#include <unordered_map>
+
+const std::vector<std::string> attributes = {
+    "wscript.exe", 
+    ".locked", 
+    "certutil.exe", 
+    "\\Startup\\",
+};
+
 
 int main(int argc, char** argv) {
     // Аргументы разбираются грубо: путь к журналу и ничего больше. Остальное,
     // включая --quiet, добавляется по заданию.
+    
+
+    bool is_quite = false;
+
     if (argc < 2) {
         std::print(stderr, "использование: nano-edr <журнал.log>\n");
         return 2;
@@ -30,8 +44,17 @@ int main(int argc, char** argv) {
         return 2;
     }
 
+    //Проверка на флаги
+    for (int i = 2; i < argc; ++i) {
+        if (std::string(argv[i]) == "--quiet") {
+            is_quite = true;
+        }
+    }
+
     long long lines = 0;
     long long comments = 0;
+    long long events = 0;
+    std::pmr::unordered_map<std::string, long long> event_types_count;
     std::string line;
 
     while (std::getline(log, line)) {
@@ -40,11 +63,29 @@ int main(int argc, char** argv) {
         // нельзя открыть файл и посмотреть.
         ++lines;
 
+
         // Строки-комментарии в журнале начинаются с '#'. Они не события,
         // и детекта по ним быть не должно.
         if (!line.empty() && line[0] == '#') {
             ++comments;
             continue;
+        } else if (!line.empty()) {
+            ++events;
+
+            //Проверка на известные признаки
+            for (auto& attribute:attributes) {
+                if (line.find(attribute) != std::string::npos) {
+                    std::print("[DETECT] строка {}, признак {}: {}\n", lines, attribute, line);
+                }
+            }
+
+            //Типизация события
+            int type_start_index = line.find("type=")+5;
+            int type_end_index = line.find(" ", type_start_index);
+            int type_len = type_end_index - type_start_index;
+            std::string type = line.substr(type_start_index, type_len);
+            ++event_types_count[type];
+
         }
 
         // >>> Здесь начинается занятие 1.1.
@@ -53,6 +94,14 @@ int main(int argc, char** argv) {
         // в выводе, — это lines.
     }
 
-    std::print("строк {}, из них комментариев {}\n", lines, comments);
+    //Необязательный вывод
+    if (!is_quite){
+        std::println("строк {}, из них комментариев {}\n", lines, comments);
+        std::println("событий всего : {}", events);
+        for (auto& [type, count]:event_types_count){
+            std::println("событий типа {} всего : {}", type, count);
+        }
+
+    }
     return 0;
 }
